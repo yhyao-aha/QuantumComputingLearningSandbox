0f8bfecd972c1ceabe3ab08311f9ed5cdd5dae8c
yhyao-aha <yiheyao@yahoo.com>
2022-11-27 09:39:10 -0800

    DFT and RSA
    Signed-off-by: yhyao-aha <yiheyao@yahoo.com>

---
+++ DFT_Julia.jl
@@ -0,0 +1,19 @@
+function DFT(x̄::Array{<:Number})
+    n = length(x̄)
+    ȳ = zeros(Complex, n)
+    
+    factor = convert(Complex, 1.0 / sqrt(n))
+    for k = 0:n-1
+        sum = 0+0im
+        for j = 0:n-1
+            τ = exp(2π*im*j*k/n)
+            sum = sum + τ * x̄[j+1]
+        end
+        ȳ[k+1] = sum * factor
+    end
+    ȳ
+end
+
+x̄ = [1 1 0im 2]
+ȳ = DFT(x̄)
+display(ȳ)
---
+++ rsa.jl
@@ -0,0 +1,38 @@
+function modpow(base::Int64, power::Int64, mod::Int64)
+    result = Int64(1)
+    for i in range(1, power, step=1)
+        result = rem((result * base), mod)
+    end
+    result
+end
+
+p= Int64(11)
+q = Int64(13)
+N = p*q
+r = (p-1)*(q-1)
+d = 2
+e = Int64(7)
+
+for i in range(2,(1<<30), step=1)
+    hit = rem(e*i, r) == 1
+    if hit
+        global d = i
+        break
+    end
+end
+
+println("public key is: ", N, ", ", e)
+println("private key is: ", N, ", ", d)
+
+m = Int64(9726)
+c = modpow(m, e, N)
+
+println("c is ", c)
+m_de = modpow(c, d, N)
+println("m is ", m, ", m_de is ", m_de)
+
+
+for i in 1:130
+    fp = modpow(e, i, N)
+    println("i is ", i," fp is ", fp)
+end
